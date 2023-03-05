cda00cd4397345e27a5e5ff508958726ada81a9e
Caioscg <cacazaok5@gmail.com>
2023-03-05 19:56:55 -0300

    notesController functions

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,3 +1,4 @@
+const { response } = require("express")
 const knex = require("../database/knex")
 
 class NotesController {
@@ -32,6 +33,59 @@ class NotesController {
 
         res.json()
 
+    }
+
+    async show(req, res) {
+        const { id } = req.params
+
+        const note = await knex("notes").where({ id }).first() //! pega as informações da nota
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name")  //! pega as tags em ordem alfabetica
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at")  //! pega as tags em ordem de criação
+
+        return res.json({
+            ...note,
+            tags,
+            links
+        })
+
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+
+        await knex("notes").where({ id }).delete()
+
+        return res.json()
+    }
+
+    async index(req, res) {
+        const { user_id, title, tags } = req.query
+
+        let notes
+
+        if (tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim())  // para unir as tags em um vetor separado por ','
+
+            notes = await knex("tags")      //! quando eu pesquiso uma tag existente ele linka mostrando as informações da note
+                .select([                                           //*? seleciona esses campos da table notes
+                    "notes.id",
+                    "notes.title",
+                    "notes.user_id"
+                ])
+                .where("notes.user_id", user_id)                    //*? busca as notas do id de user passado no query
+                .whereLike("notes.title", `%${title}%`)             //*? e dentro desse id, busca a note com o titulo digitado
+                .whereIn("name", filterTags)                        //*? compara se o nome digitado pertence ao array
+                .innerJoin("notes", "notes.id", "tags.note_id")     //*? estou juntando a tags com a notes por meio do notes_id
+                .orderBy("notes.title")
+
+        } else {
+            notes = await knex("notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)  //* achar ao pesquisar qualquer palavra do title (pesquisa na query do insomnia)
+            .orderBy("title") //* ordem alfabética
+        }
+
+        return res.json(notes)
     }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,7 +6,9 @@ const NotesControllers = require("../controllers/NotesController")
 
 const NotesController = new NotesControllers()
 
-
 notesRoutes.post("/:user_id", NotesController.create)
+notesRoutes.get("/:id", NotesController.show)
+notesRoutes.delete("/:id", NotesController.delete)
+notesRoutes.get("/", NotesController.index)
 
 module.exports = notesRoutes
