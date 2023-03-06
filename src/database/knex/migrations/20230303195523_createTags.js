635ae79ab32be11a81ea4269f5070b854c25bdf2
Caioscg <cacazaok5@gmail.com>
2023-03-06 17:40:38 -0300

    corrections

--- src/database/knex/migrations/20230303195523_createTags.js
+++ src/database/knex/migrations/20230303195523_createTags.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("tags", table => {
     table.increments("id")
     table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE") //! se deletar a nota, as tags vinculadas ao id da nota tmb é deletado
     table.integer("user_id").references("id").inTable("users")
-    table.text("name").notNullable
+    table.text("name").notNullable()
 })
 
 exports.down = knex => knex.schema.dropTable("tags")
--- src/database/knex/migrations/20230303200349_createLinks.js
+++ src/database/knex/migrations/20230303200349_createLinks.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("links", table => {
     table.increments("id")
     table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE") //! se deletar a nota, as tags vinculadas ao id da nota tmb é deletado
 
-    table.text("url").notNullable
+    table.text("url").notNullable()
     table.timestamp("created_at").default(knex.fn.now())
 })
 
