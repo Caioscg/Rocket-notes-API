27a6adcd9ab03fc41d26d07314794958e8d57772
Caioscg <cacazaok5@gmail.com>
2023-05-07 22:50:33 -0300

    API test

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -4,7 +4,7 @@ const { compare } = require("bcryptjs")
 
 class SessionsController {
     async create(req, res) {
-        const { email, password } = req.body
+        const { email, password } = req.body 
 
         const user = await knex("users").where({ email }).first()
 
