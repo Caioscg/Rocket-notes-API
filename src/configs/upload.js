d7363cccca4d5264bce47c591fdcfa11be424155
Caioscg <cacazaok5@gmail.com>
2023-05-18 23:19:42 -0300

    uploads.js

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,4 +1,24 @@
 const path = require("path")
+const multer = require("multer")  // npm install multer (p/ uploads)
+const crypto = require("crypto")
 
-const TMP_FOLDE = path.resolve(__dirname, "..", "..", "tmp")
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")  // pasta temporaria
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")     // pasta onde ficarao os uploads
+
+const MULTER = {
+    storage: multer.diskStorage({
+        destination: TMP_FOLDER,
+        filename(req, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString("hex")  // para garantir q nao vai te imgs duplicadas
+            const fileName = `${fileHash}-${file.originalname}`
+
+            return callback(null, fileName)
+        },
+    }),
+}
+
+module.exports = {
+    TMP_FOLDER,
+    UPLOADS_FOLDER,
+    MULTER
+}
