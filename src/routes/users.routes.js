96d257d344df64ea63873c4a948a08f616c38b09
Caioscg <cacazaok5@gmail.com>
2023-05-24 22:59:08 -0300

    save avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,19 +3,18 @@ const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
 const UsersControllers = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const userRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersControllers()
+const userAvatarController = new UserAvatarController()
 
 
 userRoutes.post("/", usersController.create)
 userRoutes.put("/", ensureAuthenticated, usersController.update)
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-    console.log(req.file.filename)
-    res.json()
-})  // patch p/ atualizar 1 campo em específico
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)  // patch p/ atualizar 1 campo em específico
 
 module.exports = userRoutes //* exportando para os outros arquivos
