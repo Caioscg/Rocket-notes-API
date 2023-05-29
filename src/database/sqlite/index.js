44035397352a299d9ede5565c95912aa61e7215a
Caioscg <cacazaok5@gmail.com>
2023-05-29 16:20:19 -0300

    better comments

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -2,6 +2,8 @@ const sqlite3 = require("sqlite3")
 const sqlite = require("sqlite")
 const path = require("path")
 
+//* npm install sqlite3 sqlite --save
+
 async function sqliteConection() {
     const database = await sqlite.open({
         filename: path.resolve(__dirname, "..", "database.db"),  // para salvar o banco de dados em todos os sistemas operacionais que rodar o código
