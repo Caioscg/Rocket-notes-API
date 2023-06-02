11278386977b294a8a3c56c9750d5829d8235046
Caioscg <cacazaok5@gmail.com>
2023-06-01 23:14:33 -0300

    api test

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db") 
     },
     pool: {
       afterCreate:(conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
